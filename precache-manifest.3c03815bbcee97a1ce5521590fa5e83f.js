2654936787ce150db57aa0149b5c3cdc5cad25ba
Xinyiguo2 <xinyig2@illinois.edu>
2020-03-28 02:42:44 -0500

    Updates

---
+++ precache-manifest.3c03815bbcee97a1ce5521590fa5e83f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6e5a9380dc5f7411d74be15c744941f",
+    "url": "/ece499-online-adventrue/index.html"
+  },
+  {
+    "revision": "557ef11b1fe8c3c3737a",
+    "url": "/ece499-online-adventrue/static/css/main.28890d62.chunk.css"
+  },
+  {
+    "revision": "9ca001ea08aa0bbfd819",
+    "url": "/ece499-online-adventrue/static/js/2.800c4aef.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ece499-online-adventrue/static/js/2.800c4aef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "557ef11b1fe8c3c3737a",
+    "url": "/ece499-online-adventrue/static/js/main.8ecdd4ee.chunk.js"
+  },
+  {
+    "revision": "945c982e84c41db47496",
+    "url": "/ece499-online-adventrue/static/js/runtime-main.aeec8515.js"
+  }
+]);
